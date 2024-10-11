210d4bd9220f3c25bbca8a1ef507ad405ef49e82
tom <tosanchez@frba.utn.edu.ar>
2024-10-11 14:43:22 -0300

    Initial commit

---
+++ src/template/main.py
@@ -0,0 +1,26 @@
+"""
+Applicant Main File.
+"""
+from fastapi import FastAPI
+
+from template.asgi import get_application
+
+app: FastAPI = get_application()
+
+
+if __name__ == "__main__":
+    # pylint: disable=wrong-import-position
+    import uvicorn
+
+    # pylint: disable=ungrouped-imports
+    from template.settings.uvicorn_settings import UvicornSettings
+
+    settings = UvicornSettings()
+
+    uvicorn.run(
+        "template.main:app",
+        host=str(settings.HOST),
+        port=settings.PORT,
+        log_level=settings.LOG_LEVEL,
+        reload=settings.RELOAD,
+    )
